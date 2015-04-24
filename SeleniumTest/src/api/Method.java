9f956d32ea2029be01f7030fe3e7a4b16bb0e206
Criss <Criss@192.168.1.102>
2015-04-23 20:51:45 -0500

    First commit

---
+++ SeleniumTest/src/api/Method.java
@@ -0,0 +1,12 @@
+package api;
+
+public class Method {
+
+	public Method(){
+		
+	}
+	
+	public String printSomething(String msj) {
+		return "message: " + msj + "This is a new save";
+	}
+}
---
+++ SeleniumTest/src/test/MainClass.java
@@ -0,0 +1,11 @@
+package test;
+
+import api.Method;
+
+public class MainClass {
+
+	public static void main(String[] args){
+		System.out.println("Hey this is the message:");
+	    System.out.println(new Method().printSomething("Hello World"));
+	}
+}
